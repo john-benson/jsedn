bbf65c495730846cf631d2398b97fd44dad5bbbe
Phil Hachey <phil.hachey@bluespurs.com>
2015-09-23 09:49:20 -0300

    Added <> to list of accepted symbols

--- jsedn.js
+++ jsedn.js
@@ -500,7 +500,7 @@ require.register("jsedn/lib/atoms.js", function (exports, module) {
 
     __extends(Symbol, _super);
 
-    Symbol.prototype.validRegex = /[0-9A-Za-z.*+!\-_?$%&=:#{}/]+/;
+    Symbol.prototype.validRegex = /[0-9A-Za-z.*+!\-_?$%&=<>:#{}/]+/;
 
     Symbol.prototype.invalidFirstChars = [":", "#", "/"];
 
